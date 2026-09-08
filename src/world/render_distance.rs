5a29ca07ecde4b488b150f605f6256503ff84a0d
Sara Borges <sarakborges@outlook.com>
2026-09-08 15:53:43 -0300

    Reduce render distance while stabilizing voxel renderer

--- src/world/render_distance.rs
+++ src/world/render_distance.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 
 pub const MIN_RENDER_DISTANCE_CHUNKS: i32 = 4;
-pub const MAX_RENDER_DISTANCE_CHUNKS: i32 = 12;
-pub const DEFAULT_RENDER_DISTANCE_CHUNKS: i32 = 8;
+pub const MAX_RENDER_DISTANCE_CHUNKS: i32 = 8;
+pub const DEFAULT_RENDER_DISTANCE_CHUNKS: i32 = 6;
 
 #[derive(Resource)]
 pub struct RenderDistanceSettings {
