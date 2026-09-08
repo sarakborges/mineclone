4282e0d01aabe5848ee3df4cfc27e749aa37da0d
Sara Borges <sarakborges@outlook.com>
2026-09-08 02:21:07 -0300

    feat: load blocks and sky profiles

--- src/content/loader.rs
+++ src/content/loader.rs
@@ -1,23 +1,30 @@
-use std::{fs, path::{Path, PathBuf}};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use bevy::prelude::*;
 use serde::de::DeserializeOwned;
 
 use super::{
     biome::{BiomeDefinition, BiomeRegistry},
+    block::{BlockDefinition, BlockRegistry},
     day_night_cycle::{DayNightCycleDefinition, DayNightCycleRegistry},
     dimension::{DimensionDefinition, DimensionRegistry},
+    sky::{SkyDefinition, SkyRegistry},
 };
 
-const DIMENSIONS_ROOT: &str = "assets/data/dimensions";
+const DATA_ROOT: &str = "assets/data";
 
 pub fn load_content(mut commands: Commands) {
     let mut biome_registry = BiomeRegistry::default();
+    let mut block_registry = BlockRegistry::default();
     let mut dimension_registry = DimensionRegistry::default();
     let mut day_night_cycle_registry = DayNightCycleRegistry::default();
+    let mut sky_registry = SkyRegistry::default();
     let mut files = Vec::new();
 
-    collect_ron_files(Path::new(DIMENSIONS_ROOT), &mut files);
+    collect_ron_files(Path::new(DATA_ROOT), &mut files);
 
     for path in files {
         let file_name = path.file_name().and_then(|name| name.to_str()).unwrap_or_default();
@@ -26,17 +33,26 @@ pub fn load_content(mut commands: Commands) {
             dimension_registry.insert(read_definition::<DimensionDefinition>(&path));
         } else if file_name == "day_night_cycle.ron" {
             day_night_cycle_registry.insert(read_definition::<DayNightCycleDefinition>(&path));
+        } else if file_name == "sky.ron" {
+            sky_registry.insert(read_definition::<SkyDefinition>(&path));
         } else if path
             .components()
             .any(|component| component.as_os_str().to_string_lossy() == "biomes")
         {
             biome_registry.insert(read_definition::<BiomeDefinition>(&path));
+        } else if path
+            .components()
+            .any(|component| component.as_os_str().to_string_lossy() == "blocks")
+        {
+            block_registry.insert(read_definition::<BlockDefinition>(&path));
         }
     }
 
     commands.insert_resource(biome_registry);
+    commands.insert_resource(block_registry);
     commands.insert_resource(dimension_registry);
     commands.insert_resource(day_night_cycle_registry);
+    commands.insert_resource(sky_registry);
 }
 
 fn collect_ron_files(directory: &Path, files: &mut Vec<PathBuf>) {
