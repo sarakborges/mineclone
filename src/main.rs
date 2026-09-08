f6f0a365417eba745fe745f6b80c1f69cb3297d3
Sara Borges <sarakborges@outlook.com>
2026-09-08 00:30:26 -0300

    Register componentized game modules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 mod block_targeting;
+mod crosshair;
 mod game_state;
 mod gameplay;
+mod gameplay_scene;
+mod player;
+mod player_camera;
+mod player_movement;
 mod starting_screen;
+mod target_highlight;
+mod target_hud;
+mod test_chunk;
 mod voxel_chunk;
+mod voxel_collision;
+mod voxel_mesh;
+mod voxel_raycast;
 
 use bevy::prelude::*;
 use block_targeting::BlockTargetingPlugin;
