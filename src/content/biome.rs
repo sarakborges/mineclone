6022a944125141e2ed990b310ef7ac7a3b70a618
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:25:08 -0300

    Add biome definition registry

---
+++ src/content/biome.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+use bevy::prelude::*;
+use serde::Deserialize;
+
+use super::color::Rgb;
+
+#[derive(Clone, Deserialize)]
+pub struct BiomeVisuals {
+    pub sky_color: Rgb,
+    pub fog_color: Rgb,
+    pub terrain_color: Rgb,
+    pub terrain_roughness: f32,
+    pub terrain_metallic: f32,
+}
+
+#[derive(Clone, Deserialize)]
+pub struct BiomeDefinition {
+    pub id: String,
+    pub name: String,
+    pub visuals: BiomeVisuals,
+}
+
+#[derive(Resource, Default)]
+pub struct BiomeRegistry {
+    definitions: HashMap<String, BiomeDefinition>,
+}
+
+impl BiomeRegistry {
+    pub fn insert(&mut self, definition: BiomeDefinition) {
+        self.definitions.insert(definition.id.clone(), definition);
+    }
+
+    pub fn get(&self, id: &str) -> Option<&BiomeDefinition> {
+        self.definitions.get(id)
+    }
+}
