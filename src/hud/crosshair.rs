7ae28dcd3e0ee342040a47545627cc8987d81c76
Sara Borges <sarakborges@outlook.com>
2026-09-08 13:24:32 -0300

    refactor: apply Asteria styling to crosshair

--- src/hud/crosshair.rs
+++ src/hud/crosshair.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::app::game_state::GameState;
+use crate::{
+    app::game_state::GameState,
+    ui::theme,
+};
 
 pub struct CrosshairPlugin;
 
@@ -11,24 +14,68 @@ impl Plugin for CrosshairPlugin {
 }
 
 fn spawn_crosshair(mut commands: Commands) {
-    commands.spawn((
-        Node {
-            position_type: PositionType::Absolute,
-            width: percent(100),
-            height: percent(100),
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
-            ..default()
-        },
-        Pickable::IGNORE,
-        DespawnOnExit(GameState::Gameplay),
-        children![(
-            Text::new("+"),
-            TextFont {
-                font_size: FontSize::Px(24.0),
+    commands
+        .spawn((
+            Node {
+                position_type: PositionType::Absolute,
+                width: percent(100),
+                height: percent(100),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
                 ..default()
             },
-            TextColor(Color::WHITE),
-        )],
-    ));
+            Pickable::IGNORE,
+            DespawnOnExit(GameState::Gameplay),
+        ))
+        .with_children(|root| {
+            root.spawn((
+                Node {
+                    width: px(18),
+                    height: px(18),
+                    position_type: PositionType::Relative,
+                    ..default()
+                },
+                Pickable::IGNORE,
+            ))
+            .with_children(|crosshair| {
+                crosshair.spawn((
+                    Node {
+                        position_type: PositionType::Absolute,
+                        left: px(2),
+                        top: px(8),
+                        width: px(14),
+                        height: px(2),
+                        border_radius: BorderRadius::MAX,
+                        ..default()
+                    },
+                    BackgroundColor(theme::TEXT_PRIMARY.with_alpha(0.92)),
+                    BoxShadow(vec![ShadowStyle {
+                        color: theme::CYAN_GLOW,
+                        x_offset: px(0),
+                        y_offset: px(0),
+                        spread_radius: px(0),
+                        blur_radius: px(8),
+                    }]),
+                ));
+                crosshair.spawn((
+                    Node {
+                        position_type: PositionType::Absolute,
+                        left: px(8),
+                        top: px(2),
+                        width: px(2),
+                        height: px(14),
+                        border_radius: BorderRadius::MAX,
+                        ..default()
+                    },
+                    BackgroundColor(theme::TEXT_PRIMARY.with_alpha(0.92)),
+                    BoxShadow(vec![ShadowStyle {
+                        color: theme::CYAN_GLOW,
+                        x_offset: px(0),
+                        y_offset: px(0),
+                        spread_radius: px(0),
+                        blur_radius: px(8),
+                    }]),
+                ));
+            });
+        });
 }
