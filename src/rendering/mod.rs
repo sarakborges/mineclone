23fc0d6599c42075590f6405c54394adefea3aaa
Sara Borges <sarakborges@outlook.com>
2026-09-08 02:22:18 -0300

    feat: add celestial rendering plugin

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,9 +1,11 @@
+mod celestial;
 mod environment;
 mod fog;
 mod lighting;
 mod sky;
 
 use bevy::prelude::*;
+use celestial::CelestialPlugin;
 use environment::EnvironmentPlugin;
 use fog::FogPlugin;
 use lighting::LightingPlugin;
@@ -13,6 +15,12 @@ pub struct RenderingPlugin;
 
 impl Plugin for RenderingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((EnvironmentPlugin, LightingPlugin, FogPlugin, SkyPlugin));
+        app.add_plugins((
+            EnvironmentPlugin,
+            LightingPlugin,
+            FogPlugin,
+            SkyPlugin,
+            CelestialPlugin,
+        ));
     }
 }
