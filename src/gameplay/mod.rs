657e1d030d6b0061795647b069e98f4d7cdbeb8d
Sara Borges <sarakborges@outlook.com>
2026-09-08 00:55:57 -0300

    remove obsolete gameplay scene ownership

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -1,19 +1,11 @@
-mod scene;
-
 use bevy::prelude::*;
 
 use crate::player::{camera::PlayerCameraPlugin, movement::PlayerMovementPlugin, PlayerPlugin};
-use scene::GameplayScenePlugin;
 
 pub struct GameplayPlugin;
 
 impl Plugin for GameplayPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((
-            GameplayScenePlugin,
-            PlayerPlugin,
-            PlayerCameraPlugin,
-            PlayerMovementPlugin,
-        ));
+        app.add_plugins((PlayerPlugin, PlayerCameraPlugin, PlayerMovementPlugin));
     }
 }
