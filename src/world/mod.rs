8d196ab7f7dd8661b84af4c970ad439df7686797
Sara Borges <sarakborges@outlook.com>
2026-09-08 00:54:48 -0300

    add world plugin

---
+++ src/world/mod.rs
@@ -0,0 +1,16 @@
+mod render_distance;
+mod setup;
+mod test_world;
+
+use bevy::prelude::*;
+
+use crate::app::game_state::GameState;
+use setup::setup_world;
+
+pub struct WorldPlugin;
+
+impl Plugin for WorldPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Gameplay), setup_world);
+    }
+}
