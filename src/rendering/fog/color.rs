228e499764f408d628a9f468275cc1012fd524c6
Sara Borges <sarakborges@outlook.com>
2026-09-08 15:38:12 -0300

    Separate fog color updates

---
+++ src/rendering/fog/color.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+
+use crate::{
+    player::camera::GameplayCamera,
+    rendering::environment::EnvironmentVisualState,
+};
+
+pub(super) fn update_fog_color(
+    visuals: Res<EnvironmentVisualState>,
+    mut fogs: Query<&mut DistanceFog, With<GameplayCamera>>,
+) {
+    if !visuals.is_changed() {
+        return;
+    }
+
+    for mut fog in &mut fogs {
+        fog.color = visuals.fog_color;
+    }
+}
