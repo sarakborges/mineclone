68cb076091797b463de445efe0d8f3aa260000f0
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:21:21 -0300

    Add display name to current dimension

--- src/world/dimension.rs
+++ src/world/dimension.rs
@@ -1,16 +1,19 @@
 use bevy::prelude::*;
 
 pub const DEFAULT_DIMENSION_ID: &str = "mineclone:overworld";
+pub const DEFAULT_DIMENSION_NAME: &str = "Overworld";
 
 #[derive(Resource)]
 pub struct CurrentDimension {
     pub id: &'static str,
+    pub name: &'static str,
 }
 
 impl Default for CurrentDimension {
     fn default() -> Self {
         Self {
             id: DEFAULT_DIMENSION_ID,
+            name: DEFAULT_DIMENSION_NAME,
         }
     }
 }
