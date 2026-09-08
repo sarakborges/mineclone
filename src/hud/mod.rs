7bd97178990b12c4fe35ea108bb592ae46a5e6ba
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:23:34 -0300

    Split HUD into world and chunk panels

--- src/hud/mod.rs
+++ src/hud/mod.rs
@@ -1,16 +1,23 @@
+mod chunk;
 mod crosshair;
-mod player;
 mod targeting;
+mod world;
 
 use bevy::prelude::*;
+use chunk::ChunkHudPlugin;
 use crosshair::CrosshairPlugin;
-use player::PlayerHudPlugin;
 use targeting::TargetHudPlugin;
+use world::WorldHudPlugin;
 
 pub struct HudPlugin;
 
 impl Plugin for HudPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((CrosshairPlugin, PlayerHudPlugin, TargetHudPlugin));
+        app.add_plugins((
+            CrosshairPlugin,
+            ChunkHudPlugin,
+            WorldHudPlugin,
+            TargetHudPlugin,
+        ));
     }
 }
