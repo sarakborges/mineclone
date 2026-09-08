ef193d6e0be863cf45a2c8710ba2875576262a1e
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:30:33 -0300

    Add sky color renderer

---
+++ src/rendering/sky.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+
+use crate::app::game_state::GameState;
+
+use super::environment::EnvironmentVisualState;
+
+pub struct SkyPlugin;
+
+impl Plugin for SkyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            PostUpdate,
+            update_sky_color.run_if(in_state(GameState::Gameplay)),
+        );
+    }
+}
+
+fn update_sky_color(
+    visuals: Res<EnvironmentVisualState>,
+    mut clear_color: ResMut<ClearColor>,
+) {
+    clear_color.0 = visuals.sky_color;
+}
