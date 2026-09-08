2a4f974dbf6171c65f530fdb15c3fba5014ecbc4
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:31:14 -0300

    Compose environment rendering plugins

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,14 +1,18 @@
+mod environment;
 mod fog;
 mod lighting;
+mod sky;
 
 use bevy::prelude::*;
+use environment::EnvironmentPlugin;
 use fog::FogPlugin;
 use lighting::LightingPlugin;
+use sky::SkyPlugin;
 
 pub struct RenderingPlugin;
 
 impl Plugin for RenderingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((LightingPlugin, FogPlugin));
+        app.add_plugins((EnvironmentPlugin, LightingPlugin, FogPlugin, SkyPlugin));
     }
 }
