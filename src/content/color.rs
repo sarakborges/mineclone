e31f6723ee11351ce32eb3e82ec63eef33822f98
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:24:53 -0300

    Add data-driven RGB value type

---
+++ src/content/color.rs
@@ -0,0 +1,31 @@
+use bevy::prelude::*;
+use serde::Deserialize;
+
+#[derive(Clone, Copy, Deserialize)]
+pub struct Rgb {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+impl Rgb {
+    pub fn to_color(self) -> Color {
+        Color::srgb(self.r, self.g, self.b)
+    }
+
+    pub fn lerp(self, other: Self, t: f32) -> Self {
+        Self {
+            r: self.r + (other.r - self.r) * t,
+            g: self.g + (other.g - self.g) * t,
+            b: self.b + (other.b - self.b) * t,
+        }
+    }
+
+    pub fn multiply(self, other: Self) -> Self {
+        Self {
+            r: self.r * other.r,
+            g: self.g * other.g,
+            b: self.b * other.b,
+        }
+    }
+}
