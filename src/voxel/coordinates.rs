4db3a8b184954c94362775875d9e51ffeefbb3b8
Sara Borges <sarakborges@outlook.com>
2026-09-08 15:09:37 -0300

    chore: remove unused local chunk coordinates

--- src/voxel/coordinates.rs
+++ src/voxel/coordinates.rs
@@ -5,7 +5,6 @@ use super::chunk::CHUNK_SIZE;
 #[derive(Clone, Copy)]
 pub struct ChunkCoordinates {
     pub chunk: IVec3,
-    pub local: Vec3,
 }
 
 pub fn split_dimension_position(position: Vec3) -> ChunkCoordinates {
@@ -15,10 +14,6 @@ pub fn split_dimension_position(position: Vec3) -> ChunkCoordinates {
         (position.y / chunk_size).floor() as i32,
         (position.z / chunk_size).floor() as i32,
     );
-    let chunk_origin = chunk.as_vec3() * chunk_size;
 
-    ChunkCoordinates {
-        chunk,
-        local: position - chunk_origin,
-    }
+    ChunkCoordinates { chunk }
 }
