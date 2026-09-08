2c1781bb0125e85a40f2ae26b1b313643bb8d72c
Sara Borges <sarakborges@outlook.com>
2026-09-08 00:54:08 -0300

    add circular render distance radius

---
+++ src/world/render_distance.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+
+pub const RENDER_DISTANCE_RADIUS: i32 = 8;
+
+pub fn chunk_coords_in_radius(center: IVec2, radius: i32) -> Vec<IVec2> {
+    let mut coords = Vec::new();
+    let radius_squared = radius * radius;
+
+    for z in -radius..=radius {
+        for x in -radius..=radius {
+            if x * x + z * z <= radius_squared {
+                coords.push(center + IVec2::new(x, z));
+            }
+        }
+    }
+
+    coords
+}
