a80540a9654a0d42af51c1b914140d0c6647c07b
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:28:40 -0300

    Store current biome by content id

--- src/world/biome.rs
+++ src/world/biome.rs
@@ -1,19 +1,16 @@
 use bevy::prelude::*;
 
 pub const DEFAULT_BIOME_ID: &str = "mineclone:overworld/plains";
-pub const DEFAULT_BIOME_NAME: &str = "Plains";
 
 #[derive(Resource)]
 pub struct CurrentBiome {
-    pub id: &'static str,
-    pub name: &'static str,
+    pub id: String,
 }
 
 impl Default for CurrentBiome {
     fn default() -> Self {
         Self {
-            id: DEFAULT_BIOME_ID,
-            name: DEFAULT_BIOME_NAME,
+            id: DEFAULT_BIOME_ID.to_owned(),
         }
     }
 }
