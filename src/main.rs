02f187ca66cc9bf979cfe544f0369fe02cb660aa
Sara Borges <sarakborges@outlook.com>
2026-09-08 14:27:41 -0300

    Load Windows icon from embedded executable resource

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,10 @@ use screens::{
 use targeting::block::BlockTargetingPlugin;
 use ui::UiDesignSystemPlugin;
 #[cfg(target_os = "windows")]
-use winit::platform::windows::WindowExtWindows;
+use winit::{
+    dpi::PhysicalSize,
+    platform::windows::{IconExtWindows, WindowExtWindows},
+};
 use winit::window::Icon;
 use world::WorldPlugin;
 
@@ -71,20 +74,21 @@ fn set_window_icon(mut icon_set: Local<bool>) {
             return;
         }
 
-        let icon_rgba = image::load_from_memory(include_bytes!(
-            "../assets/branding/asteria_icon.png"
-        ))
-        .expect("Asteria window icon should be a valid PNG")
-        .into_rgba8();
-        let (width, height) = icon_rgba.dimensions();
-        let icon = Icon::from_rgba(icon_rgba.into_raw(), width, height)
-            .expect("Asteria window icon should have valid RGBA dimensions");
+        #[cfg(target_os = "windows")]
+        let window_icon = Icon::from_resource(1, Some(PhysicalSize::new(32, 32)))
+            .unwrap_or_else(|_| load_png_icon());
+        #[cfg(not(target_os = "windows"))]
+        let window_icon = load_png_icon();
+
+        #[cfg(target_os = "windows")]
+        let taskbar_icon = Icon::from_resource(1, Some(PhysicalSize::new(256, 256)))
+            .unwrap_or_else(|_| load_png_icon());
 
         for window in windows.windows.values() {
-            window.set_window_icon(Some(icon.clone()));
+            window.set_window_icon(Some(window_icon.clone()));
 
             #[cfg(target_os = "windows")]
-            window.set_taskbar_icon(Some(icon.clone()));
+            window.set_taskbar_icon(Some(taskbar_icon.clone()));
 
             applied = true;
         }
@@ -93,4 +97,16 @@ fn set_window_icon(mut icon_set: Local<bool>) {
     if applied {
         *icon_set = true;
     }
+}
+
+fn load_png_icon() -> Icon {
+    let icon_rgba = image::load_from_memory(include_bytes!(
+        "../assets/branding/asteria_icon.png"
+    ))
+    .expect("Asteria window icon should be a valid PNG")
+    .into_rgba8();
+    let (width, height) = icon_rgba.dimensions();
+
+    Icon::from_rgba(icon_rgba.into_raw(), width, height)
+        .expect("Asteria window icon should have valid RGBA dimensions")
 }
