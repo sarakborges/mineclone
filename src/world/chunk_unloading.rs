085eef96b787d9aa6ad7d9057c44a6fdc35d7c8e
Sara Borges <sarakborges@outlook.com>
2026-09-08 15:50:12 -0300

    Unload chunk meshes through reusable render pool

--- src/world/chunk_unloading.rs
+++ src/world/chunk_unloading.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 
 use super::{
-    chunk_rendering::RenderedChunk,
+    chunk_rendering::ChunkRenderPool,
     render_distance::RenderDistanceSettings,
 };
 
@@ -15,23 +15,29 @@ pub fn unload_chunk_meshes(
     mut meshes: ResMut<Assets<Mesh>>,
     player: Single<&Transform, With<GameplayCamera>>,
     render_distance: Res<RenderDistanceSettings>,
-    rendered_chunks: Query<(Entity, &RenderedChunk, Option<&Mesh3d>)>,
+    mut render_pool: ResMut<ChunkRenderPool>,
 ) {
     let feet_position = player.translation - Vec3::Y * PLAYER_EYE_HEIGHT;
     let player_chunk = split_dimension_position(feet_position).chunk;
     let radius = render_distance.chunks();
     let radius_squared = radius * radius;
+    let to_unload = render_pool
+        .active_coords()
+        .filter(|coord| {
+            let dx = coord.x - player_chunk.x;
+            let dz = coord.z - player_chunk.z;
+            dx * dx + dz * dz > radius_squared
+        })
+        .collect::<Vec<_>>();
 
-    for (entity, rendered_chunk, mesh) in &rendered_chunks {
-        let dx = rendered_chunk.coord.x - player_chunk.x;
-        let dz = rendered_chunk.coord.z - player_chunk.z;
-
-        if dx * dx + dz * dz <= radius_squared {
+    for coord in to_unload {
+        let Some((entity, mesh_handle)) = render_pool.take(coord) else {
             continue;
-        }
+        };
 
-        if let Some(mesh) = mesh {
-            let _ = meshes.remove(mesh.id());
+        if let Some(mesh_handle) = mesh_handle {
+            let _ = meshes.remove(&mesh_handle);
+            render_pool.recycle_mesh_handle(mesh_handle);
         }
 
         commands.entity(entity).despawn();
