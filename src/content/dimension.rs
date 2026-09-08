b6204ff30e2be0f0e2f97f9a93623f6d9b8c5181
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:25:18 -0300

    Add dimension definition registry

---
+++ src/content/dimension.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+use bevy::prelude::*;
+use serde::Deserialize;
+
+#[derive(Clone, Deserialize)]
+pub struct DimensionDefinition {
+    pub id: String,
+    pub name: String,
+    pub biomes: Vec<String>,
+    pub day_night_cycle: String,
+}
+
+#[derive(Resource, Default)]
+pub struct DimensionRegistry {
+    definitions: HashMap<String, DimensionDefinition>,
+}
+
+impl DimensionRegistry {
+    pub fn insert(&mut self, definition: DimensionDefinition) {
+        self.definitions.insert(definition.id.clone(), definition);
+    }
+
+    pub fn get(&self, id: &str) -> Option<&DimensionDefinition> {
+        self.definitions.get(id)
+    }
+}
