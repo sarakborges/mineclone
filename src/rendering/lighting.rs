ec968f9d2d3999ee84640efc6c99a4ce58793902
Sara Borges <sarakborges@outlook.com>
2026-09-08 01:30:52 -0300

    Drive lighting from environment state

--- src/rendering/lighting.rs
+++ src/rendering/lighting.rs
@@ -2,15 +2,44 @@ use bevy::prelude::*;
 
 use crate::app::game_state::GameState;
 
+use super::environment::EnvironmentVisualState;
+
 pub struct LightingPlugin;
 
 impl Plugin for LightingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Gameplay), setup_lighting);
+        app.add_systems(OnEnter(GameState::Gameplay), spawn_sun)
+            .add_systems(
+                PostUpdate,
+                update_lighting.run_if(in_state(GameState::Gameplay)),
+            );
     }
 }
 
-fn setup_lighting(mut ambient_light: ResMut<GlobalAmbientLight>) {
-    ambient_light.color = Color::srgb(0.9, 0.94, 1.0);
-    ambient_light.brightness = 450.0;
+#[derive(Component)]
+struct SunLight;
+
+fn spawn_sun(mut commands: Commands) {
+    commands.spawn((
+        DirectionalLight {
+            shadow_maps_enabled: true,
+            ..default()
+        },
+        Transform::default(),
+        SunLight,
+        DespawnOnExit(GameState::Gameplay),
+    ));
+}
+
+fn update_lighting(
+    visuals: Res<EnvironmentVisualState>,
+    mut ambient_light: ResMut<GlobalAmbientLight>,
+    mut sun: Single<(&mut DirectionalLight, &mut Transform), With<SunLight>>,
+) {
+    ambient_light.color = visuals.ambient_color;
+    ambient_light.brightness = visuals.ambient_brightness;
+
+    sun.0.color = visuals.sun_color;
+    sun.0.illuminance = visuals.sun_illuminance;
+    sun.1.rotation = visuals.sun_rotation;
 }
