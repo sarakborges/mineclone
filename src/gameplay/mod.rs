2da36d621c21c610799324bd20a516300e1b2420
Sara Borges <sarakborges@outlook.com>
2026-09-08 20:52:36 -0300

    Enable player hotbar input

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -2,13 +2,24 @@ mod pause;
 
 use bevy::prelude::*;
 
-use crate::player::{camera::PlayerCameraPlugin, movement::PlayerMovementPlugin, PlayerPlugin};
+use crate::player::{
+    camera::PlayerCameraPlugin,
+    hotbar::PlayerHotbarPlugin,
+    movement::PlayerMovementPlugin,
+    PlayerPlugin,
+};
 use pause::PausePlugin;
 
 pub struct GameplayPlugin;
 
 impl Plugin for GameplayPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((PausePlugin, PlayerPlugin, PlayerCameraPlugin, PlayerMovementPlugin));
+        app.add_plugins((
+            PausePlugin,
+            PlayerPlugin,
+            PlayerHotbarPlugin,
+            PlayerCameraPlugin,
+            PlayerMovementPlugin,
+        ));
     }
 }
