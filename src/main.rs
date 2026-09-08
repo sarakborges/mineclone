a8def97c456d80bfa399b08aa1bdae0ac9500571
Sara Borges <sarakborges@outlook.com>
2026-09-08 00:56:41 -0300

    wire world and rendering plugins

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ mod app;
 mod gameplay;
 mod hud;
 mod player;
+mod rendering;
 mod screens;
 mod targeting;
 mod voxel;
+mod world;
 
 use app::game_state::GameState;
 use bevy::prelude::*;
 use gameplay::GameplayPlugin;
 use hud::HudPlugin;
+use rendering::RenderingPlugin;
 use screens::starting_screen::StartingScreenPlugin;
 use targeting::block::BlockTargetingPlugin;
+use world::WorldPlugin;
 
 fn main() {
     App::new()
@@ -26,7 +30,9 @@ fn main() {
         .insert_resource(ClearColor(Color::srgb(0.02, 0.025, 0.04)))
         .add_plugins((
             StartingScreenPlugin,
+            WorldPlugin,
             GameplayPlugin,
+            RenderingPlugin,
             BlockTargetingPlugin,
             HudPlugin,
         ))
