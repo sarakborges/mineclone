88ceba8dde6ee243f2e956d93963efb0b7fe35b2
Sara Borges <sarakborges@outlook.com>
2026-09-08 21:21:02 -0300

    Reuse shared block model rendering

--- src/player/viewmodel.rs
+++ src/player/viewmodel.rs
@@ -1,14 +1,9 @@
-use bevy::{
-    asset::RenderAssetUsages,
-    light::NotShadowCaster,
-    mesh::Indices,
-    prelude::*,
-    render::render_resource::PrimitiveTopology,
-};
+use bevy::{light::NotShadowCaster, prelude::*};
 
 use crate::{
     app::game_state::GameState,
-    content::block::{BlockDefinition, BlockRegistry},
+    content::block::BlockRegistry,
+    rendering::block_model::{block_face_material, block_face_mesh, block_faces},
     voxel::mesh::BlockFace,
 };
 
@@ -108,6 +103,7 @@ fn spawn_viewmodel(
                                     block,
                                     &asset_server,
                                     &mut materials,
+                                    1.0,
                                 );
 
                                 held.spawn((
@@ -160,83 +156,8 @@ fn sync_held_block(
                 block,
                 &asset_server,
                 &mut materials,
+                1.0,
             );
         }
     }
-}
-
-fn block_face_material(
-    face: BlockFace,
-    block: &BlockDefinition,
-    asset_server: &AssetServer,
-    materials: &mut Assets<StandardMaterial>,
-) -> Handle<StandardMaterial> {
-    let texture = match face {
-        BlockFace::Right => &block.textures.right,
-        BlockFace::Left => &block.textures.left,
-        BlockFace::Top => &block.textures.top,
-        BlockFace::Bottom => &block.textures.bottom,
-        BlockFace::Front => &block.textures.front,
-        BlockFace::Back => &block.textures.back,
-    };
-
-    materials.add(StandardMaterial {
-        base_color: Color::WHITE,
-        base_color_texture: Some(asset_server.load(texture.clone())),
-        perceptual_roughness: 1.0,
-        unlit: true,
-        ..default()
-    })
-}
-
-fn block_faces() -> [BlockFace; 6] {
-    [
-        BlockFace::Right,
-        BlockFace::Left,
-        BlockFace::Top,
-        BlockFace::Bottom,
-        BlockFace::Front,
-        BlockFace::Back,
-    ]
-}
-
-fn block_face_mesh(face: BlockFace) -> Mesh {
-    let (vertices, normal) = match face {
-        BlockFace::Right => (
-            [[0.5, -0.5, 0.5], [0.5, -0.5, -0.5], [0.5, 0.5, -0.5], [0.5, 0.5, 0.5]],
-            [1.0, 0.0, 0.0],
-        ),
-        BlockFace::Left => (
-            [[-0.5, -0.5, -0.5], [-0.5, -0.5, 0.5], [-0.5, 0.5, 0.5], [-0.5, 0.5, -0.5]],
-            [-1.0, 0.0, 0.0],
-        ),
-        BlockFace::Top => (
-            [[-0.5, 0.5, 0.5], [0.5, 0.5, 0.5], [0.5, 0.5, -0.5], [-0.5, 0.5, -0.5]],
-            [0.0, 1.0, 0.0],
-        ),
-        BlockFace::Bottom => (
-            [[-0.5, -0.5, -0.5], [0.5, -0.5, -0.5], [0.5, -0.5, 0.5], [-0.5, -0.5, 0.5]],
-            [0.0, -1.0, 0.0],
-        ),
-        BlockFace::Front => (
-            [[-0.5, -0.5, 0.5], [0.5, -0.5, 0.5], [0.5, 0.5, 0.5], [-0.5, 0.5, 0.5]],
-            [0.0, 0.0, 1.0],
-        ),
-        BlockFace::Back => (
-            [[0.5, -0.5, -0.5], [-0.5, -0.5, -0.5], [-0.5, 0.5, -0.5], [0.5, 0.5, -0.5]],
-            [0.0, 0.0, -1.0],
-        ),
-    };
-
-    Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::RENDER_WORLD,
-    )
-    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices.to_vec())
-    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![normal; 4])
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![[0.0, 1.0], [1.0, 1.0], [1.0, 0.0], [0.0, 0.0]],
-    )
-    .with_inserted_indices(Indices::U32(vec![0, 1, 2, 0, 2, 3]))
 }
