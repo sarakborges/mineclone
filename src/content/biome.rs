712502611223423e48879c850870b55b20d45b7e
Sara Borges <sarakborges@outlook.com>
2026-09-09 20:36:41 -0300

    Add hydrology-only biome identity kind

--- src/content/biome.rs
+++ src/content/biome.rs
@@ -18,16 +18,17 @@ pub enum BiomeKind {
     #[default]
     Surface,
     Volume,
+    Hydrology,
 }
 
-#[derive(Clone, Copy, Deserialize)]
+#[derive(Clone, Copy, Default, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BiomeSizeAxis {
     pub min: f32,
     pub max: f32,
 }
 
-#[derive(Clone, Copy, Deserialize)]
+#[derive(Clone, Copy, Default, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BiomeSize {
     pub x: BiomeSizeAxis,
@@ -92,6 +93,7 @@ pub struct BiomeDefinition {
     pub name: String,
     #[serde(default)]
     pub kind: BiomeKind,
+    #[serde(default)]
     pub size: BiomeSize,
     #[serde(default)]
     pub climate: BiomeClimate,
@@ -117,15 +119,14 @@ pub struct BiomeRegistry {
 
 impl BiomeRegistry {
     pub fn insert(&mut self, definition: BiomeDefinition) {
-        validate_size_axis(&definition.id, "x", definition.size.x);
-        validate_size_axis(&definition.id, "z", definition.size.z);
-
-        if let Some(vertical_size) = definition.size.y {
-            validate_size_axis(&definition.id, "y", vertical_size);
-        }
-
         match definition.kind {
             BiomeKind::Surface => {
+                validate_size_axis(&definition.id, "x", definition.size.x);
+                validate_size_axis(&definition.id, "z", definition.size.z);
+                if let Some(vertical_size) = definition.size.y {
+                    validate_size_axis(&definition.id, "y", vertical_size);
+                }
+
                 assert!(
                     definition.terrain.is_some(),
                     "surface biome {} must define terrain",
@@ -143,9 +144,37 @@ impl BiomeRegistry {
                 );
             }
             BiomeKind::Volume => {
+                validate_size_axis(&definition.id, "x", definition.size.x);
+                validate_size_axis(&definition.id, "z", definition.size.z);
+                let vertical_size = definition.size.y.unwrap_or_else(|| {
+                    panic!("volume biome {} must define size.y", definition.id)
+                });
+                validate_size_axis(&definition.id, "y", vertical_size);
+            }
+            BiomeKind::Hydrology => {
                 assert!(
-                    definition.size.y.is_some(),
-                    "volume biome {} must define size.y",
+                    definition.terrain.is_none(),
+                    "hydrology biome {} cannot define terrain",
+                    definition.id
+                );
+                assert!(
+                    definition.density_modifier.is_none(),
+                    "hydrology biome {} cannot define densityModifier",
+                    definition.id
+                );
+                assert!(
+                    definition.solid_block.is_none(),
+                    "hydrology biome {} cannot define solidBlock",
+                    definition.id
+                );
+                assert!(
+                    definition.vertical_range.is_none(),
+                    "hydrology biome {} cannot define verticalRange",
+                    definition.id
+                );
+                assert_eq!(
+                    definition.priority, 0,
+                    "hydrology biome {} cannot define volume overlap priority",
                     definition.id
                 );
             }
