de30a4f7f44cbdba24b2d0f84a380a64bdc12227
Sara Borges <sarakborges@outlook.com>
2026-09-10 19:34:45 -0300

    Register glass block id

--- src/content/builtin_ids.rs
+++ src/content/builtin_ids.rs
@@ -5,6 +5,7 @@ pub const SAND_BLOCK_ID: &str = "asteria:sand";
 pub const LAMP_BLOCK_ID: &str = "asteria:lamp";
 pub const OAK_WOOD_BLOCK_ID: &str = "asteria:oak_wood";
 pub const OAK_LEAF_BLOCK_ID: &str = "asteria:oak_leaf";
+pub const GLASS_BLOCK_ID: &str = "asteria:glass";
 pub const WATER_FLUID_ID: &str = "asteria:water";
 pub const OVERWORLD_DIMENSION_ID: &str = "asteria:overworld";
 pub const PLAINS_BIOME_ID: &str = "asteria:overworld/plains";
