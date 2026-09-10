61b8f4d478b02c078658f2e84c8de6b2b09e0957
Sara Borges <sarakborges@outlook.com>
2026-09-10 17:41:28 -0300

    Decouple fluid spread range from visual levels

--- src/content/fluid.rs
+++ src/content/fluid.rs
@@ -6,7 +6,6 @@ use serde::Deserialize;
 use super::color::Rgb;
 
 pub type FluidId = u16;
-pub const MAX_FLUID_SPREAD: u8 = 7;
 
 #[derive(Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -21,7 +20,7 @@ pub struct FluidDefinition {
     #[serde(default)]
     pub light_dampening: u8,
     pub spread_speed: f32,
-    pub max_spread: u8,
+    pub max_spread: u16,
 }
 
 #[derive(Resource, Default)]
@@ -60,11 +59,6 @@ impl FluidRegistry {
         assert!(
             definition.spread_speed.is_finite() && definition.spread_speed >= 0.0,
             "fluid {} spread speed must be finite and non-negative",
-            definition.id
-        );
-        assert!(
-            definition.max_spread <= MAX_FLUID_SPREAD,
-            "fluid {} max spread must be between 0 and {MAX_FLUID_SPREAD}",
             definition.id
         );
 
