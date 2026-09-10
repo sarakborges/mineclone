4642b4b831c14377ab37482ad144ca3ef4a7f5d1
Sara Borges <sarakborges@outlook.com>
2026-09-10 15:58:01 -0300

    Add sand builtin id

--- src/content/builtin_ids.rs
+++ src/content/builtin_ids.rs
@@ -1,6 +1,7 @@
 pub const GRASS_BLOCK_ID: &str = "asteria:grass";
 pub const DIRT_BLOCK_ID: &str = "asteria:dirt";
 pub const STONE_BLOCK_ID: &str = "asteria:stone";
+pub const SAND_BLOCK_ID: &str = "asteria:sand";
 pub const LAMP_BLOCK_ID: &str = "asteria:lamp";
 pub const WATER_FLUID_ID: &str = "asteria:water";
 pub const OVERWORLD_DIMENSION_ID: &str = "asteria:overworld";
