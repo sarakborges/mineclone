984f15c68c05d579b70119d756d14617ac352789
Sara Borges <sarakborges@outlook.com>
2026-09-10 15:58:12 -0300

    Add sand to default hotbar

--- src/player/hotbar.rs
+++ src/player/hotbar.rs
@@ -2,7 +2,9 @@ use bevy::prelude::*;
 
 use crate::{
     app::{game_state::GameState, pause_state::PauseState},
-    content::builtin_ids::{DIRT_BLOCK_ID, GRASS_BLOCK_ID, LAMP_BLOCK_ID, STONE_BLOCK_ID},
+    content::builtin_ids::{
+        DIRT_BLOCK_ID, GRASS_BLOCK_ID, LAMP_BLOCK_ID, SAND_BLOCK_ID, STONE_BLOCK_ID,
+    },
 };
 
 pub const HOTBAR_SLOT_COUNT: usize = 9;
@@ -20,6 +22,7 @@ impl Default for PlayerHotbar {
         slots[1] = Some(DIRT_BLOCK_ID);
         slots[2] = Some(STONE_BLOCK_ID);
         slots[3] = Some(LAMP_BLOCK_ID);
+        slots[4] = Some(SAND_BLOCK_ID);
 
         Self {
             selected_slot: 0,
