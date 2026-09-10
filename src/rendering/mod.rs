6252c122f9801a4e8400be117cb81a10204f1429
Sara Borges <sarakborges@outlook.com>
2026-09-10 14:10:01 -0300

    Initialize shared block model assets

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -14,7 +14,7 @@ pub(crate) mod terrain_material;
 
 use asset_upload::AssetUploadPlugin;
 use bevy::prelude::*;
-use block_model::setup_block_model_meshes;
+use block_model::setup_block_model_assets;
 use celestial::CelestialPlugin;
 use directional_shadows::DirectionalShadowsPlugin;
 use dynamic_lights::DynamicLightsPlugin;
@@ -30,7 +30,7 @@ pub struct RenderingPlugin;
 impl Plugin for RenderingPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(MaterialPlugin::<TerrainMaterial>::default())
-            .add_systems(Startup, setup_block_model_meshes)
+            .add_systems(Startup, setup_block_model_assets)
             .add_plugins((
                 AssetUploadPlugin,
                 EnvironmentPlugin,
