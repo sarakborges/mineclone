d701db0cb0378a87bf371454e218b12ef2887126
Sara Borges <sarakborges@outlook.com>
2026-09-11 20:39:46 -0300

    Align chunk rendering visibility

--- src/world/chunk_rendering.rs
+++ src/world/chunk_rendering.rs
@@ -10,17 +10,17 @@ use crate::{
 
 use super::biome_field::BiomeField;
 
-pub use materials::{FluidMaterials, TerrainMaterials};
-pub use pool::{ChunkRenderPool, clear_chunk_render_pool};
-pub use refresh::refresh_chunk_mesh;
-pub use spawn::spawn_chunk_mesh;
+pub(crate) use materials::{FluidMaterials, TerrainMaterials};
+pub(crate) use pool::{ChunkRenderPool, clear_chunk_render_pool};
+pub(crate) use refresh::refresh_chunk_mesh;
+pub(crate) use spawn::spawn_chunk_mesh;
 
 pub(crate) struct ChunkRenderContext<'a> {
-    pub world: &'a VoxelWorld,
-    pub blocks: &'a BlockRegistry,
-    pub fluids: &'a FluidRegistry,
-    pub biomes: &'a BiomeRegistry,
-    pub biome_field: &'a BiomeField,
-    pub terrain_materials: &'a TerrainMaterials,
-    pub fluid_materials: &'a FluidMaterials,
+    pub(crate) world: &'a VoxelWorld,
+    pub(crate) blocks: &'a BlockRegistry,
+    pub(crate) fluids: &'a FluidRegistry,
+    pub(crate) biomes: &'a BiomeRegistry,
+    pub(crate) biome_field: &'a BiomeField,
+    pub(crate) terrain_materials: &'a TerrainMaterials,
+    pub(crate) fluid_materials: &'a FluidMaterials,
 }
