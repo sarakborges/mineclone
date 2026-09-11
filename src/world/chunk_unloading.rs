fbc2cbedd6b6722ad1f5a795fb7b116e3c577d00
Sara Borges <sarakborges@outlook.com>
2026-09-10 23:27:42 -0300

    Unload chunks from streaming desired set

--- src/world/chunk_unloading.rs
+++ src/world/chunk_unloading.rs
@@ -11,14 +11,14 @@ use crate::{
 use super::{
     chunk_remesh::ChunkRemeshQueue,
     chunk_system_params::ChunkRenderer,
-    render_distance::{RenderDistanceSettings, chunk_is_in_volume},
+    streaming::ChunkStreamingState,
 };
 
 const MAX_CHUNK_UNLOADS_PER_FRAME: usize = 2;
 
 pub fn unload_chunk_meshes(
     player: Single<&Transform, With<GameplayCamera>>,
-    render_distance: Res<RenderDistanceSettings>,
+    streaming: Res<ChunkStreamingState>,
     mut renderer: ChunkRenderer,
     mut world: ResMut<VoxelWorld>,
     mut lighting: ResMut<PendingLightingUpdates>,
@@ -27,12 +27,10 @@ pub fn unload_chunk_meshes(
     let feet_position = player.translation - Vec3::Y * PLAYER_EYE_HEIGHT;
     let player_chunk = split_dimension_position(feet_position).chunk;
     let center = IVec3::new(player_chunk.x, player_chunk.y.max(0), player_chunk.z);
-    let horizontal_radius = render_distance.chunks();
-    let vertical_radius = render_distance.vertical_chunks();
     let mut to_unload = renderer
         .pool
         .active_coords()
-        .filter(|coord| !chunk_is_in_volume(center, *coord, horizontal_radius, vertical_radius))
+        .filter(|coord| !streaming.wants(*coord))
         .collect::<Vec<_>>();
 
     to_unload.sort_by_key(|coord| -(*coord - center).length_squared());
