e9630a97083fb898fa9df04bdf0c48cce2086302
Sara Borges <sarakborges@outlook.com>
2026-09-12 15:33:23 -0300

    Restore simplified sun shading

--- src/rendering/directional_shadows.rs
+++ src/rendering/directional_shadows.rs
@@ -44,7 +44,7 @@ fn spawn_sun_shadow_light(mut commands: Commands) {
     commands.spawn((
         DirectionalLight {
             illuminance: 0.0,
-            shadow_maps_enabled: true,
+            shadow_maps_enabled: false,
             ..default()
         },
         CascadeShadowConfigBuilder {
@@ -95,7 +95,7 @@ fn update_sun_shadow_light(
     for (mut light, mut transform, mut visibility) in &mut lights {
         light.color = sky.sun.tint.to_color();
         light.illuminance = BASE_SUN_ILLUMINANCE * sample.sky_light_factor;
-        light.shadow_maps_enabled = true;
+        light.shadow_maps_enabled = false;
         transform.rotation = rotation;
         *visibility = Visibility::Visible;
     }
