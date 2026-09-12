8fc4184064f3d3f2346f10eb5ac1ffe07a3ee8bc
Sara Borges <sarakborges@outlook.com>
2026-09-12 04:13:03 -0300

    Use survival as default game mode

--- src/player/game_mode.rs
+++ src/player/game_mode.rs
@@ -2,8 +2,8 @@ use bevy::prelude::*;
 
 #[derive(Component, Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
 pub(crate) enum GameMode {
-    #[default]
     Creative,
+    #[default]
     Survival,
 }
 
