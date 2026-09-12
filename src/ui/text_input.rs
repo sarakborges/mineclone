29d375f819621d574ba192975c6b2a253fcba814
Sara Borges <sarakborges@outlook.com>
2026-09-12 12:25:43 -0300

    Make select all shortcut order independent

--- src/ui/text_input.rs
+++ src/ui/text_input.rs
@@ -1,6 +1,11 @@
 use bevy::prelude::*;
 
 pub(crate) fn select_all_pressed(keys: &ButtonInput<KeyCode>) -> bool {
-    keys.any_pressed([KeyCode::ControlLeft, KeyCode::ControlRight])
-        && keys.just_pressed(KeyCode::KeyA)
+    let control_pressed =
+        keys.pressed(KeyCode::ControlLeft) || keys.pressed(KeyCode::ControlRight);
+    let control_just_pressed =
+        keys.just_pressed(KeyCode::ControlLeft) || keys.just_pressed(KeyCode::ControlRight);
+
+    (control_pressed && keys.just_pressed(KeyCode::KeyA))
+        || (keys.pressed(KeyCode::KeyA) && control_just_pressed)
 }
