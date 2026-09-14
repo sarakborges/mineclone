f54e859cca66c0069eef2ebeb09c24039195446c
Sara Borges <sarakborges@outlook.com>
2026-09-14 13:21:19 -0300

    Remove transitional dyed property reexport

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -3,7 +3,6 @@ mod brush;
 use bevy::prelude::*;
 use brush::BrushPlugin;
 
-pub(crate) use crate::content::builtin_ids::DYED_PROPERTY_ID;
 pub(crate) use brush::{BrushMode, BrushPaletteState};
 
 pub(crate) struct ToolsPlugin;
