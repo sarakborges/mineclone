7a93971fec49766c6cc8f46c5b9095589c94aaeb
Sara Borges <sarakborges@outlook.com>
2026-09-14 13:15:48 -0300

    Own dyed property ID in content catalog

--- src/content/builtin_ids.rs
+++ src/content/builtin_ids.rs
@@ -2,3 +2,4 @@ pub const BRUSH_TOOL_ID: &str = "asteria:brush";
 pub const WATER_FLUID_ID: &str = "asteria:water";
 pub const OVERWORLD_DIMENSION_ID: &str = "asteria:overworld";
 pub const PLAINS_BIOME_ID: &str = "asteria:overworld/plains";
+pub const DYED_PROPERTY_ID: &str = "dyed";
