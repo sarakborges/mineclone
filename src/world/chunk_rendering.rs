e9d4ab65bdc6a61c08aa8bc67a234eb4970e57c5
Sara Borges <sarakborges@outlook.com>
2026-09-14 14:10:15 -0300

    Separate CPU chunk meshing from render spawning

--- src/world/chunk_rendering.rs
+++ src/world/chunk_rendering.rs
@@ -8,7 +8,7 @@ use crate::{
         biome::BiomeRegistry, block::BlockRegistry, fluid::FluidRegistry,
         secondary_property::SecondaryPropertyRegistry,
     },
-    voxel::world::VoxelWorld,
+    voxel::{read::VoxelRead, world::VoxelWorld},
 };
 
 use super::biome_field::BiomeField;
@@ -18,7 +18,18 @@ pub(crate) use pool::{ChunkRenderPool, clear_chunk_render_pool};
 pub(crate) use refresh::{
     refresh_chunk_fluid_mesh, refresh_chunk_geometry_mesh, refresh_chunk_lighting_mesh,
 };
-pub(crate) use spawn::spawn_chunk_mesh;
+pub(crate) use spawn::{
+    BuiltChunkMesh, build_chunk_render_meshes, spawn_built_chunk_meshes, spawn_chunk_mesh,
+};
+
+pub(crate) struct ChunkMeshBuildContext<'a, W: VoxelRead + ?Sized> {
+    pub(crate) world: &'a W,
+    pub(crate) blocks: &'a BlockRegistry,
+    pub(crate) fluids: &'a FluidRegistry,
+    pub(crate) biomes: &'a BiomeRegistry,
+    pub(crate) secondary_properties: &'a SecondaryPropertyRegistry,
+    pub(crate) biome_field: &'a BiomeField,
+}
 
 pub(crate) struct ChunkRenderContext<'a> {
     pub(crate) world: &'a VoxelWorld,
@@ -29,4 +40,17 @@ pub(crate) struct ChunkRenderContext<'a> {
     pub(crate) biome_field: &'a BiomeField,
     pub(crate) terrain_materials: &'a TerrainMaterials,
     pub(crate) fluid_materials: &'a FluidMaterials,
+}
+
+impl ChunkRenderContext<'_> {
+    pub(crate) fn mesh_build_context(&self) -> ChunkMeshBuildContext<'_, VoxelWorld> {
+        ChunkMeshBuildContext {
+            world: self.world,
+            blocks: self.blocks,
+            fluids: self.fluids,
+            biomes: self.biomes,
+            secondary_properties: self.secondary_properties,
+            biome_field: self.biome_field,
+        }
+    }
 }
