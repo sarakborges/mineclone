c8003c567b076c46eb3e54dd21625215e2baf447
Sara Borges <sarakborges@outlook.com>
2026-09-14 12:05:39 -0300

    Centralize voxel edit side effects

---
+++ src/voxel/edit.rs
@@ -0,0 +1,55 @@
+use bevy::{ecs::system::SystemParam, prelude::*};
+
+use crate::world::{chunk_remesh::ChunkRemeshQueue, fluid_updates::PendingFluidUpdates};
+
+use super::{cell::VoxelCell, lighting::PendingLightingUpdates, world::VoxelWorld};
+
+#[derive(SystemParam)]
+pub(crate) struct VoxelMutationRuntime<'w> {
+    world: ResMut<'w, VoxelWorld>,
+    lighting: ResMut<'w, PendingLightingUpdates>,
+    remesh_queue: ResMut<'w, ChunkRemeshQueue>,
+}
+
+impl VoxelMutationRuntime<'_> {
+    pub(crate) fn world(&self) -> &VoxelWorld {
+        &self.world
+    }
+
+    pub(crate) fn cell_at(&self, world_position: IVec3) -> Option<VoxelCell> {
+        self.world.cell_at(world_position)
+    }
+
+    pub(crate) fn set_block(
+        &mut self,
+        world_position: IVec3,
+        block: Option<VoxelCell>,
+    ) -> Option<IVec3> {
+        let chunk = self.world.set_block_at(world_position, block)?;
+        self.lighting.enqueue_voxel_edit(world_position);
+        self.remesh_queue.enqueue_voxel_edit(chunk);
+        Some(chunk)
+    }
+}
+
+#[derive(SystemParam)]
+pub(crate) struct VoxelTopologyRuntime<'w> {
+    mutation: VoxelMutationRuntime<'w>,
+    fluid_updates: ResMut<'w, PendingFluidUpdates>,
+}
+
+impl VoxelTopologyRuntime<'_> {
+    pub(crate) fn world(&self) -> &VoxelWorld {
+        self.mutation.world()
+    }
+
+    pub(crate) fn set_block(
+        &mut self,
+        world_position: IVec3,
+        block: Option<VoxelCell>,
+    ) -> Option<IVec3> {
+        let chunk = self.mutation.set_block(world_position, block)?;
+        self.fluid_updates.enqueue_voxel_edit(world_position);
+        Some(chunk)
+    }
+}
