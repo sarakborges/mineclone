f6e1b743aedf09beaa9765d52b0d54555f7be320
Sara Borges <sarakborges@outlook.com>
2026-09-14 12:45:40 -0300

    Register shared biome visual context

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -4,6 +4,7 @@ pub(crate) mod block_model;
 pub(crate) mod block_model_material;
 pub(crate) mod block_texture;
 pub(crate) mod block_tint;
+mod biome_visuals;
 mod celestial;
 mod celestial_path;
 pub(crate) mod color;
