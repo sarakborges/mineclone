c7d9c34c6683f9e03a8e50a59ea1ecd624770901
Sara Borges <sarakborges@outlook.com>
2026-09-14 00:54:10 -0300

    Blend biome visuals in HSI

--- src/world/biome_field/visuals.rs
+++ src/world/biome_field/visuals.rs
@@ -1,95 +1,50 @@
 use bevy::prelude::*;
 
-use crate::content::{biome::BiomeRegistry, color::Rgb};
+use crate::content::{biome::BiomeRegistry, color::Hsi};
 
 use super::BiomeField;
 
 impl BiomeField {
-    pub fn grass_color(&self, position: Vec2, biomes: &BiomeRegistry) -> Rgb {
+    pub fn grass_color(&self, position: Vec2, biomes: &BiomeRegistry) -> Hsi {
         let sample = self.sample_surface(position);
-        let mut color = Rgb {
-            r: 0.0,
-            g: 0.0,
-            b: 0.0,
-        };
-
-        for influence in sample.influences {
+        Hsi::blend_weighted(sample.influences.into_iter().map(|influence| {
             let biome = biomes
                 .get(influence.id)
                 .unwrap_or_else(|| panic!("missing biome definition: {}", influence.id));
-            let grass = biome.visuals.grass_color;
-
-            color.r += grass.r * influence.weight;
-            color.g += grass.g * influence.weight;
-            color.b += grass.b * influence.weight;
-        }
-
-        color
+            (biome.visuals.grass_color, influence.weight)
+        }))
     }
 
-    pub fn leaf_color(&self, position: Vec2, biomes: &BiomeRegistry) -> Rgb {
+    pub fn leaf_color(&self, position: Vec2, biomes: &BiomeRegistry) -> Hsi {
         let sample = self.sample_surface(position);
-        let mut color = Rgb {
-            r: 0.0,
-            g: 0.0,
-            b: 0.0,
-        };
-
-        for influence in sample.influences {
+        Hsi::blend_weighted(sample.influences.into_iter().map(|influence| {
             let biome = biomes
                 .get(influence.id)
                 .unwrap_or_else(|| panic!("missing biome definition: {}", influence.id));
-            let leaf = biome.visuals.leaf_color;
-
-            color.r += leaf.r * influence.weight;
-            color.g += leaf.g * influence.weight;
-            color.b += leaf.b * influence.weight;
-        }
-
-        color
+            (biome.visuals.leaf_color, influence.weight)
+        }))
     }
 
-    pub fn foliage_color(&self, position: Vec2, biomes: &BiomeRegistry) -> Rgb {
+    pub fn foliage_color(&self, position: Vec2, biomes: &BiomeRegistry) -> Hsi {
         let sample = self.sample_surface(position);
-        let mut color = Rgb {
-            r: 0.0,
-            g: 0.0,
-            b: 0.0,
-        };
-
-        for influence in sample.influences {
+        Hsi::blend_weighted(sample.influences.into_iter().map(|influence| {
             let biome = biomes
                 .get(influence.id)
                 .unwrap_or_else(|| panic!("missing biome definition: {}", influence.id));
-            let foliage = biome.visuals.foliage_color;
-
-            color.r += foliage.r * influence.weight;
-            color.g += foliage.g * influence.weight;
-            color.b += foliage.b * influence.weight;
-        }
-
-        color
+            (biome.visuals.foliage_color, influence.weight)
+        }))
     }
 
-    pub fn water_color(&self, position: Vec2, biomes: &BiomeRegistry, fallback: Rgb) -> Rgb {
+    pub fn water_color(&self, position: Vec2, biomes: &BiomeRegistry, fallback: Hsi) -> Hsi {
         let sample = self.sample_surface(position);
-        let mut color = Rgb {
-            r: 0.0,
-            g: 0.0,
-            b: 0.0,
-        };
-
-        for influence in sample.influences {
+        Hsi::blend_weighted(sample.influences.into_iter().map(|influence| {
             let biome = biomes
                 .get(influence.id)
                 .unwrap_or_else(|| panic!("missing biome definition: {}", influence.id));
-            let water = biome.visuals.water_color.unwrap_or(fallback);
-
-            color.r += water.r * influence.weight;
-            color.g += water.g * influence.weight;
-            color.b += water.b * influence.weight;
-        }
-
-        color
+            (
+                biome.visuals.water_color.unwrap_or(fallback),
+                influence.weight,
+            )
+        }))
     }
 }
