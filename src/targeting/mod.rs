4a240554582e8a755bcbd914d5d842f784250ee8
Sara Borges <sarakborges@outlook.com>
2026-09-14 12:15:00 -0300

    Expose targeting scene context

--- src/targeting/mod.rs
+++ src/targeting/mod.rs
@@ -4,6 +4,8 @@ mod interaction;
 mod placement;
 mod placement_orientation;
 mod placement_preview;
+mod scene;
 
 pub(crate) use interaction::{ToolUse, ToolUseButton};
 pub(crate) use placement_orientation::PlacementOrientation;
+pub(crate) use scene::BlockTargetingScene;
