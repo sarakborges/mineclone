86a47acc5bccfc3d6d948cb63b02b1c4572ecfa0
Sara Borges <sarakborges@outlook.com>
2026-09-14 23:19:04 -0300

    Stop exporting obsolete lighting mesh wrapper

--- src/world/chunk_rendering.rs
+++ src/world/chunk_rendering.rs
@@ -15,9 +15,7 @@ use super::biome_field::BiomeField;
 
 pub(crate) use materials::{FluidMaterials, TerrainMaterials};
 pub(crate) use pool::{ChunkRenderPool, clear_chunk_render_pool};
-pub(crate) use refresh::{
-    refresh_chunk_fluid_mesh, refresh_chunk_geometry_mesh, refresh_chunk_lighting_mesh,
-};
+pub(crate) use refresh::{refresh_chunk_fluid_mesh, refresh_chunk_geometry_mesh};
 pub(crate) use spawn::{
     BuiltChunkMesh, build_chunk_render_meshes, spawn_built_chunk_meshes, spawn_chunk_mesh,
 };
