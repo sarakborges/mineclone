c1967984a1b4b53ef4a52a37b75b1f165b3c201d
Sara Borges <sarakborges@outlook.com>
2026-09-14 23:23:41 -0300

    Make coherent voxel sampling the read primitive

--- src/voxel/read.rs
+++ src/voxel/read.rs
@@ -3,10 +3,29 @@ use bevy::prelude::*;
 use super::{cell::VoxelCell, fluid::FluidCell, light::VoxelLight, world::VoxelWorld};
 
 pub(crate) trait VoxelRead: Send + Sync {
-    fn cell_at(&self, world_position: IVec3) -> Option<VoxelCell>;
-    fn fluid_at(&self, world_position: IVec3) -> Option<FluidCell>;
-    fn light_at(&self, world_position: IVec3) -> VoxelLight;
-    fn is_loaded_at(&self, world_position: IVec3) -> bool;
+    fn sample_at(
+        &self,
+        world_position: IVec3,
+    ) -> Option<(Option<VoxelCell>, Option<FluidCell>, VoxelLight)>;
+
+    fn cell_at(&self, world_position: IVec3) -> Option<VoxelCell> {
+        self.sample_at(world_position)
+            .and_then(|(cell, _, _)| cell)
+    }
+
+    fn fluid_at(&self, world_position: IVec3) -> Option<FluidCell> {
+        self.sample_at(world_position)
+            .and_then(|(_, fluid, _)| fluid)
+    }
+
+    fn light_at(&self, world_position: IVec3) -> VoxelLight {
+        self.sample_at(world_position)
+            .map_or(VoxelLight::DARK, |(_, _, light)| light)
+    }
+
+    fn is_loaded_at(&self, world_position: IVec3) -> bool {
+        self.sample_at(world_position).is_some()
+    }
 
     fn is_solid(&self, world_position: IVec3) -> bool {
         self.cell_at(world_position).is_some()
@@ -18,19 +37,10 @@ pub(crate) trait VoxelRead: Send + Sync {
 }
 
 impl VoxelRead for VoxelWorld {
-    fn cell_at(&self, world_position: IVec3) -> Option<VoxelCell> {
-        VoxelWorld::cell_at(self, world_position)
-    }
-
-    fn fluid_at(&self, world_position: IVec3) -> Option<FluidCell> {
-        VoxelWorld::fluid_at(self, world_position)
-    }
-
-    fn light_at(&self, world_position: IVec3) -> VoxelLight {
-        VoxelWorld::light_at(self, world_position)
-    }
-
-    fn is_loaded_at(&self, world_position: IVec3) -> bool {
-        VoxelWorld::is_loaded_at(self, world_position)
+    fn sample_at(
+        &self,
+        world_position: IVec3,
+    ) -> Option<(Option<VoxelCell>, Option<FluidCell>, VoxelLight)> {
+        VoxelWorld::sample_at(self, world_position)
     }
 }
