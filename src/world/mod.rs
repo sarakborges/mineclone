28d8bfd192ab3c3d8a2a3f46d1c0b8a36eec90ba
Sara Borges <sarakborges@outlook.com>
2026-09-14 21:47:29 -0300

    Register shared chunk task queue

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -6,6 +6,7 @@ mod chunk_mesh_tasks;
 pub(crate) mod chunk_remesh;
 pub(crate) mod chunk_rendering;
 pub(crate) mod chunk_system_params;
+mod chunk_task_queue;
 mod chunk_unloading;
 pub(crate) mod current_context;
 pub(crate) mod day_night;
