fbe301fd20ed5436f6dd2d935d2e7a6d23a02ca4
Sara Borges <sarakborges@outlook.com>
2026-09-16 01:33:05 -0300

    Remove obsolete voxel queue helper

--- src/voxel/update_queue.rs
+++ src/voxel/update_queue.rs
@@ -28,13 +28,6 @@ impl VoxelUpdateQueue {
         self.queue.enqueue_front(position);
     }
 
-    pub(crate) fn enqueue_with_neighbors(&mut self, position: IVec3) {
-        self.enqueue(position);
-        for offset in CARDINAL_NEIGHBORS {
-            self.enqueue(position + offset);
-        }
-    }
-
     pub(crate) fn enqueue_with_neighbors_priority(&mut self, position: IVec3) {
         for offset in CARDINAL_NEIGHBORS {
             self.enqueue_priority(position + offset);
@@ -62,22 +55,6 @@ impl VoxelUpdateQueue {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn edit_enqueue_includes_nonnegative_neighbors_once() {
-        let mut queue = VoxelUpdateQueue::default();
-        queue.enqueue_with_neighbors(IVec3::ZERO);
-        queue.enqueue_with_neighbors(IVec3::ZERO);
-
-        assert_eq!(queue.len(), 6);
-        let mut positions = (0..queue.len())
-            .filter_map(|_| queue.pop())
-            .collect::<Vec<_>>();
-        positions.sort_by_key(|position| (position.x, position.y, position.z));
-
-        assert!(positions.contains(&IVec3::ZERO));
-        assert!(!positions.contains(&IVec3::NEG_Y));
-    }
 
     #[test]
     fn priority_edit_puts_the_edited_voxel_first() {
