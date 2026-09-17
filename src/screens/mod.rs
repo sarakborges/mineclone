0a40ba41a64e5ac52e2f3c634b6c6622967d9ab2
Sara Borges <sarakborges@outlook.com>
2026-09-17 02:45:21 -0300

    feat(ui): wire Load Worlds screen plugin

--- src/screens/mod.rs
+++ src/screens/mod.rs
@@ -2,12 +2,14 @@ mod loading_screen;
 mod pause_menu;
 mod settings_screen;
 mod starting_screen;
+mod world_selection;
 
 use bevy::prelude::*;
 use loading_screen::LoadingScreenPlugin;
 use pause_menu::PauseMenuPlugin;
 use settings_screen::SettingsScreenPlugin;
 use starting_screen::StartingScreenPlugin;
+use world_selection::WorldSelectionPlugin;
 
 pub(crate) struct ScreensPlugin;
 
@@ -16,6 +18,7 @@ impl Plugin for ScreensPlugin {
         app.add_plugins((
             SettingsScreenPlugin,
             StartingScreenPlugin,
+            WorldSelectionPlugin,
             LoadingScreenPlugin,
             PauseMenuPlugin,
         ));
