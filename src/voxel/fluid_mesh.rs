a811de3e326d9e878c694dd7b30c9c98b0fc2b74
Sara Borges <sarakborges@outlook.com>
2026-09-17 19:56:41 -0300

    Fix Chisel-clipped fluid face geometry

--- src/voxel/fluid_mesh.rs
+++ src/voxel/fluid_mesh.rs
@@ -173,30 +173,71 @@ fn fluid_micro_face_vertices(
     min_v: f32,
     max_v: f32,
 ) -> [[f32; 3]; 4] {
-    let point = |u: f32, v: f32| -> [f32; 3] {
-        let height = bilinear_height(heights, u, v);
-        match face {
-            BlockFace::Right => [x0 + 1.0, y0 + if v == 0.0 { 0.0 } else { height }, z0 + 1.0 - u],
-            BlockFace::Left => [x0, y0 + if v == 0.0 { 0.0 } else { height }, z0 + u],
-            BlockFace::Top => [x0 + u, y0 + height, z0 + v],
-            BlockFace::Bottom => [x0 + u, y0, z0 + v],
-            BlockFace::Front => [x0 + u, y0 + if v == 0.0 { 0.0 } else { height }, z0 + 1.0],
-            BlockFace::Back => [x0 + 1.0 - u, y0 + if v == 0.0 { 0.0 } else { height }, z0],
-        }
+    let side_point = |u: f32, v: f32| -> (f32, f32) {
+        (u, bilinear_height(heights, u, v))
     };
+
     match face {
-        BlockFace::Right => [
-            point(max_v, min_u), point(min_v, min_u), point(min_v, max_u), point(max_v, max_u),
+        BlockFace::Right => {
+            let (z0a, h0a) = side_point(min_u, min_v);
+            let (z1a, h1a) = side_point(max_u, min_v);
+            let (z0b, h0b) = side_point(min_u, max_v);
+            let (z1b, h1b) = side_point(max_u, max_v);
+            [
+                [x0 + 1.0, y0, z0 + 1.0 - z1a],
+                [x0 + 1.0, y0, z0 + 1.0 - z0a],
+                [x0 + 1.0, y0 + h0b, z0 + 1.0 - z0b],
+                [x0 + 1.0, y0 + h1b, z0 + 1.0 - z1b],
+            ]
+        }
+        BlockFace::Left => {
+            let (z0a, _) = side_point(min_u, min_v);
+            let (z1a, _) = side_point(max_u, min_v);
+            let (z0b, h0b) = side_point(min_u, max_v);
+            let (z1b, h1b) = side_point(max_u, max_v);
+            [
+                [x0, y0, z0 + z0a],
+                [x0, y0, z0 + z1a],
+                [x0, y0 + h1b, z0 + z1b],
+                [x0, y0 + h0b, z0 + z0b],
+            ]
+        }
+        BlockFace::Top => [
+            [x0 + min_u, y0 + bilinear_height(heights, min_u, max_v), z0 + max_v],
+            [x0 + max_u, y0 + bilinear_height(heights, max_u, max_v), z0 + max_v],
+            [x0 + max_u, y0 + bilinear_height(heights, max_u, min_v), z0 + min_v],
+            [x0 + min_u, y0 + bilinear_height(heights, min_u, min_v), z0 + min_v],
         ],
-        BlockFace::Left => [
-            point(min_v, min_u), point(max_v, min_u), point(max_v, max_u), point(min_v, max_u),
+        BlockFace::Bottom => [
+            [x0 + min_u, y0, z0 + min_v],
+            [x0 + max_u, y0, z0 + min_v],
+            [x0 + max_u, y0, z0 + max_v],
+            [x0 + min_u, y0, z0 + max_v],
         ],
-        BlockFace::Top | BlockFace::Bottom | BlockFace::Front => [
-            point(min_u, max_v), point(max_u, max_v), point(max_u, min_v), point(min_u, min_v),
-        ],
-        BlockFace::Back => [
-            point(max_u, max_v), point(min_u, max_v), point(min_u, min_v), point(max_u, min_v),
-        ],
+        BlockFace::Front => {
+            let (x0a, _) = side_point(min_u, min_v);
+            let (x1a, _) = side_point(max_u, min_v);
+            let (_, h0b) = side_point(min_u, max_v);
+            let (_, h1b) = side_point(max_u, max_v);
+            [
+                [x0 + x0a, y0, z0 + 1.0],
+                [x0 + x1a, y0, z0 + 1.0],
+                [x0 + x1a, y0 + h1b, z0 + 1.0],
+                [x0 + x0a, y0 + h0b, z0 + 1.0],
+            ]
+        }
+        BlockFace::Back => {
+            let (x0a, _) = side_point(min_u, min_v);
+            let (x1a, _) = side_point(max_u, min_v);
+            let (_, h0b) = side_point(min_u, max_v);
+            let (_, h1b) = side_point(max_u, max_v);
+            [
+                [x0 + 1.0 - x1a, y0, z0],
+                [x0 + 1.0 - x0a, y0, z0],
+                [x0 + 1.0 - x0a, y0 + h0b, z0],
+                [x0 + 1.0 - x1a, y0 + h1b, z0],
+            ]
+        }
     }
 }
 
