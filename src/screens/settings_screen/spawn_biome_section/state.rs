f39be98d48a4cc663aaefccfe183ad9adb983cc1
Sara Borges <sarakborges@outlook.com>
2026-09-18 13:45:55 -0300

    Use shared dropdown state for biome selection

--- src/screens/settings_screen/spawn_biome_section/state.rs
+++ src/screens/settings_screen/spawn_biome_section/state.rs
@@ -1,24 +1,5 @@
-use bevy::prelude::*;
+use crate::ui::dropdown::DropdownState;
 
-#[derive(Resource, Default)]
-pub(in crate::screens::settings_screen) struct SpawnBiomeDropdownState {
-    pub(super) open: bool,
-}
-
-impl SpawnBiomeDropdownState {
-    pub(in crate::screens::settings_screen) fn input_editing(&self) -> bool {
-        self.open
-    }
-
-    pub(in crate::screens::settings_screen) fn reset(&mut self) {
-        self.open = false;
-    }
-
-    pub(in crate::screens::settings_screen) fn open(&mut self) {
-        self.open = true;
-    }
-
-    pub(in crate::screens::settings_screen) fn close(&mut self) {
-        self.open = false;
-    }
-}
+pub(in crate::screens::settings_screen) struct SpawnBiomeDropdownKind;
+pub(in crate::screens::settings_screen) type SpawnBiomeDropdownState =
+    DropdownState<SpawnBiomeDropdownKind>;
