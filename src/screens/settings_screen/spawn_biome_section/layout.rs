479d1a287667a7353558dbd5328e6bd135f695e7
Sara Borges <sarakborges@outlook.com>
2026-09-17 23:23:30 -0300

    Normalize new world field spacing

--- src/screens/settings_screen/spawn_biome_section/layout.rs
+++ src/screens/settings_screen/spawn_biome_section/layout.rs
@@ -64,7 +64,7 @@ pub(in crate::screens::settings_screen) fn spawn_biome_setting(
             width: percent(100),
             flex_direction: FlexDirection::Column,
             align_items: AlignItems::Stretch,
-            row_gap: px(10),
+            row_gap: px(8),
             ..default()
         },
         children![
