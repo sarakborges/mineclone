b454b54d607a04cdf2bb0cd4846e5dfbd2aa5a17
Sara Borges <sarakborges@outlook.com>
2026-09-17 21:34:00 -0300

    Initialize default player attack

--- src/content/loader.rs
+++ src/content/loader.rs
@@ -62,7 +62,7 @@ pub fn load_content(mut commands: Commands) {
 }
 
 pub(crate) fn read_content() -> LoadedContent {
-    let mut content = LoadedContent { player: PlayerDefinition { health: 20.0, model: None }, ..Default::default() };
+    let mut content = LoadedContent { player: PlayerDefinition { health: 20.0, attack: "asteria:punch".to_owned(), model: None }, ..Default::default() };
     let mut files = Vec::new();
 
     collect_json_files(&data_root(), &mut files);
