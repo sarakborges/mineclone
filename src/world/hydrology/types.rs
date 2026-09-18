df77645b29bbf2e3ef7bbd1ef60e838898fc3c4a
Sara Borges <sarakborges@outlook.com>
2026-09-18 18:06:32 -0300

    cleanup: remove hydrology surface overlay weight

--- src/world/hydrology/types.rs
+++ src/world/hydrology/types.rs
@@ -118,7 +118,6 @@ pub struct HydrologySurfaceSample {
 
 #[derive(Clone, Copy, Debug)]
 pub struct HydrologyBiomeOverlay<'a> {
-    pub surface_weight: f32,
     pub coast_biome: Option<&'a str>,
     pub coast_weight: f32,
     pub ocean_biome: Option<&'a str>,
