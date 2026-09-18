fdd1cdb96c1cc9496687260e1eef88d4c88a3cd1
Sara Borges <sarakborges@outlook.com>
2026-09-17 21:36:20 -0300

    Remove entity caps from biome spawn rules

--- src/content/biome.rs
+++ src/content/biome.rs
@@ -93,17 +93,11 @@ pub struct CreatureSpawnRule {
     pub light_max: u8,
     #[serde(default = "default_spawn_spacing")]
     pub spacing: f32,
-    #[serde(default = "default_spawn_max_per_type")]
-    pub max_per_type: usize,
-    #[serde(default = "default_spawn_max_in_dimension")]
-    pub max_in_dimension: usize,
 }
 
 fn default_spawn_weight() -> f32 { 1.0 }
 fn default_spawn_light_max() -> u8 { 15 }
 fn default_spawn_spacing() -> f32 { 16.0 }
-fn default_spawn_max_per_type() -> usize { 4 }
-fn default_spawn_max_in_dimension() -> usize { 16 }
 
 #[derive(Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
