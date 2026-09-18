dfabbb6b103adbf880ff7b11960e1e3ae42bc4fd
Sara Borges <sarakborges@outlook.com>
2026-09-17 21:37:13 -0300

    Track dimension entity counts by type

--- src/world/dimension.rs
+++ src/world/dimension.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use bevy::prelude::*;
 
 use crate::content::builtin_ids::OVERWORLD_DIMENSION_ID;
@@ -14,5 +16,26 @@ impl Default for CurrentDimension {
         Self {
             id: DEFAULT_DIMENSION_ID.to_owned(),
         }
+    }
+}
+
+#[derive(Resource, Default)]
+pub(crate) struct DimensionEntityCounts {
+    pub(crate) total: usize,
+    pub(crate) entities: HashMap<String, usize>,
+}
+
+impl DimensionEntityCounts {
+    pub(crate) fn rebuild<'a>(&mut self, creatures: impl Iterator<Item = &'a crate::creatures::CreatureInstance>) {
+        self.total = 0;
+        self.entities.clear();
+        for creature in creatures {
+            self.total += 1;
+            *self.entities.entry(creature.definition_id.clone()).or_default() += 1;
+        }
+    }
+
+    pub(crate) fn count(&self, entity_id: &str) -> usize {
+        self.entities.get(entity_id).copied().unwrap_or(0)
     }
 }
