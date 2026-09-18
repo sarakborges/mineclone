36dd3cf8e20cd9ec9252473f382b6a8b9db352b9
Sara Borges <sarakborges@outlook.com>
2026-09-17 23:22:25 -0300

    Differentiate selected selectable labels

--- src/ui/selectable.rs
+++ src/ui/selectable.rs
@@ -52,5 +52,5 @@ pub(crate) fn selectable_button_background(active: bool, interaction: Interactio
 }
 
 pub(crate) fn selectable_label_color(active: bool) -> Color {
-    if active { theme::TEXT_PRIMARY } else { theme::TEXT_PRIMARY }
+    if active { theme::TEXT_PRIMARY } else { theme::TEXT_MUTED }
 }
