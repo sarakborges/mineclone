9d9057fa477f3a6f86cde7a5e782dcd2491ffbcf
Sara Borges <sarakborges@outlook.com>
2026-09-18 18:44:21 -0300

    feat: register surface biome margin content

--- src/content/mod.rs
+++ src/content/mod.rs
@@ -8,6 +8,7 @@ pub(crate) mod biome_sky_layer;
 pub(crate) mod biome_structure;
 pub(crate) mod biome_surface_carver;
 pub(crate) mod biome_surface_fluid;
+pub(crate) mod biome_surface_margin;
 pub(crate) mod biome_terrain;
 pub(crate) mod biome_terrain_modifier;
 pub(crate) mod block;
