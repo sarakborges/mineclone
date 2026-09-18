558caeb2945bf15b8de2ec58c45f4d5344b2e55b
Sara Borges <sarakborges@outlook.com>
2026-09-18 11:21:59 -0300

    Keep player HUD hidden while paused

--- src/hud/player.rs
+++ src/hud/player.rs
@@ -30,12 +30,14 @@ impl Plugin for PlayerHudPlugin {
             .add_systems(
                 Update,
                 sync_inventory_hint.run_if(in_state(GameState::Gameplay)),
-            );
+            )
+            .add_systems(Update, sync_player_hud_pause_visibility.run_if(in_state(GameState::Gameplay)));
     }
 }
 
 #[derive(Component)]
 struct PlayerHudRoot;
+
 
 #[derive(Component)]
 struct InventoryHint;
