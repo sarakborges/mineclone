8eddfdb21739c0add9ac7b451f3e3af98ba46285
Sara Borges <sarakborges@outlook.com>
2026-09-19 14:04:25 -0300

    Remove speculative terrain texture readiness check

--- src/world/chunk_rendering/materials.rs
+++ src/world/chunk_rendering/materials.rs
@@ -71,12 +71,6 @@ impl TerrainMaterials {
         }
     }
 
-    pub(crate) fn textures_ready(&self, images: &Assets<Image>) -> bool {
-        self._texture_preloads
-            .iter()
-            .all(|handle| images.contains(handle))
-    }
-
     pub(super) fn for_face(
         &self,
         block_id: &str,
