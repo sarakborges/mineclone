dd24716cac7ed922eabcb8ced5b9151fd1f73e2e
Sara Borges <sarakborges@outlook.com>
2026-09-18 22:36:04 -0300

    Remove superseded player save wrapper

--- src/player/save.rs
+++ src/player/save.rs
@@ -22,10 +22,6 @@ impl PlayerSaveData {
         self.health
     }
 
-    pub(crate) fn save(&mut self, position: Vec3, game_mode: GameMode) {
-        self.save_with_health(position, game_mode, None);
-    }
-
     pub(crate) fn save_with_health(
         &mut self,
         position: Vec3,
