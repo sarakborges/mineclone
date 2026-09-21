ef563f2f541197180e7329078c7b7cb758fc84e5
Sara Borges <sarakborges@outlook.com>
2026-09-21 10:40:15 -0300

    fix: restore layer placement world mutation API

--- src/voxel/world.rs
+++ src/voxel/world.rs
@@ -7,7 +7,7 @@ use bevy::{
     prelude::*,
 };
 
-use crate::content::layer::LayerFace;
+use crate::content::layer::{LayerFace, LayerRegistry};
 
 use super::{
     cell::VoxelCell,
@@ -15,6 +15,7 @@ use super::{
     chunk_archive::ArchivedChunk,
     coordinates::{chunk_coord_from_world, split_world_position},
     fluid::FluidCell,
+    layer::LayerCell,
     light::VoxelLight,
 };
 
@@ -363,6 +364,42 @@ impl VoxelWorld {
         Some((chunk_coord, previous_block))
     }
 
+
+    pub(crate) fn add_layer_at(
+        &mut self,
+        world_position: IVec3,
+        face: LayerFace,
+        layer: LayerCell,
+        registry: &LayerRegistry,
+    ) -> Option<IVec3> {
+        if world_position.y < 0 {
+            return None;
+        }
+        let definition = registry.get(layer.layer_id)?;
+        if !definition.supports_face(face) {
+            return None;
+        }
+
+        let (chunk_coord, local_position) = split_world_position(world_position);
+        let changed = {
+            let chunk = self.chunks.get_mut(&chunk_coord)?;
+            chunk.add_layer(
+                local_position.x as usize,
+                local_position.y as usize,
+                local_position.z as usize,
+                face,
+                layer,
+            )
+        };
+        if !changed {
+            return None;
+        }
+
+        self.persistent_chunks.insert(chunk_coord);
+        self.bump_chunk_content_revision(chunk_coord);
+        self.bump_chunk_mesh_revision(chunk_coord);
+        Some(chunk_coord)
+    }
 
     pub(crate) fn remove_top_layer_at(
         &mut self,
