54ff385c4aa0f7dc299b1fd51aec3be7cb5d48a3
Sara Borges <sarakborges@outlook.com>
2026-09-21 13:39:57 -0300

    Enable structure tool plugin

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,11 +1,13 @@
 mod brush;
 mod chisel;
 mod shears;
+mod structure_tool;
 
 use bevy::prelude::*;
 use brush::BrushPlugin;
 use chisel::ChiselPlugin;
 use shears::ShearsPlugin;
+use structure_tool::StructureToolPlugin;
 
 pub(crate) use brush::{BrushMode, BrushPaletteState};
 
@@ -13,6 +15,6 @@ pub(crate) struct ToolsPlugin;
 
 impl Plugin for ToolsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((BrushPlugin, ChiselPlugin, ShearsPlugin));
+        app.add_plugins((BrushPlugin, ChiselPlugin, ShearsPlugin, StructureToolPlugin));
     }
 }
