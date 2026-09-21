3b13b2a9111b152e1cd06617784e880651287535
Sara Borges <sarakborges@outlook.com>
2026-09-21 20:55:17 -0300

    Use adaptive lighting cache for full fluid meshes

--- src/voxel/fluid_mesh.rs
+++ src/voxel/fluid_mesh.rs
@@ -41,14 +41,17 @@ where
     W: VoxelRead + ?Sized,
     F: FnMut(IVec3, FluidId) -> [f32; 3],
 {
-    let lighting_cache =
-        ChunkLightingCache::capture(world, chunk_coord * CHUNK_SIZE as i32);
+    let lighting_cache = ChunkLightingCache::capture_if_worthwhile(
+        world,
+        chunk_coord * CHUNK_SIZE as i32,
+        chunk,
+    );
     build_fluid_meshlets(
         world,
         chunk_coord,
         chunk,
         ChunkMeshletMask::ALL,
-        Some(&lighting_cache),
+        lighting_cache.as_ref(),
         tint_at,
     )
 }
