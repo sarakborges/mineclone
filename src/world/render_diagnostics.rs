df3bde98a67cbfa4d64a46205495068eb676014e
Sara Borges <sarakborges@outlook.com>
2026-09-21 19:10:16 -0300

    Log async generation and meshing timings

--- src/world/render_diagnostics.rs
+++ src/world/render_diagnostics.rs
@@ -80,6 +80,7 @@ pub(super) fn log_render_asset_pressure(
     let generation_tasks = assets.generation_tasks.pending_count();
     let async_chunk_work = assets.async_work.in_flight();
     let async_chunk_work_limit = assets.async_work.limit();
+    let async_timings = assets.async_work.take_diagnostics();
     let mesh_tasks = assets.mesh_tasks.pending_count();
     let remesh_tasks = assets.remesh_tasks.pending_count();
     let (remesh_geometry, remesh_lighting, remesh_fluid) =
@@ -135,8 +136,11 @@ pub(super) fn log_render_asset_pressure(
     });
 
     info!(
-        "render assets: state={:?} active_chunks={active_chunks} pooled_meshes={pooled_meshes} render_entities={render_entities} terrain_array_meshes={terrain_array_meshes} terrain_legacy_meshes={terrain_legacy_meshes} layer_meshes={layer_meshes} fluid_meshes={fluid_meshes} pooled_mesh_bytes={pooled_mesh_bytes} stream_pending={stream_pending} stream_ready={stream_ready} generation_tasks={generation_tasks} async_chunk_work={async_chunk_work}/{async_chunk_work_limit} generation_wave_pending={generation_wave_pending} generation_wave_targets={generation_wave_targets} staged_generated_chunks={staged_generated_chunks} pressure_evicted_meshes={pressure_evicted_meshes} mesh_tasks={mesh_tasks} remesh_tasks={remesh_tasks} remesh_geometry={remesh_geometry} remesh_lighting={remesh_lighting} remesh_fluid={remesh_fluid} mesh_assets={mesh_assets} images={image_assets} file_images={file_images} runtime_images={runtime_images} non_font_runtime_images={non_font_runtime_images} runtime_top_shapes={runtime_top_shapes:?} font_atlas_keys={font_atlas_keys} font_atlases={font_atlas_count} font_atlas_bytes={font_atlas_bytes} deltas={deltas:?} standard_materials={} terrain_materials={}",
+        "render assets: state={:?} active_chunks={active_chunks} pooled_meshes={pooled_meshes} render_entities={render_entities} terrain_array_meshes={terrain_array_meshes} terrain_legacy_meshes={terrain_legacy_meshes} layer_meshes={layer_meshes} fluid_meshes={fluid_meshes} pooled_mesh_bytes={pooled_mesh_bytes} stream_pending={stream_pending} stream_ready={stream_ready} generation_tasks={generation_tasks} async_chunk_work={async_chunk_work}/{async_chunk_work_limit} async_generation={:?} async_initial_mesh={:?} async_remesh={:?} generation_wave_pending={generation_wave_pending} generation_wave_targets={generation_wave_targets} staged_generated_chunks={staged_generated_chunks} pressure_evicted_meshes={pressure_evicted_meshes} mesh_tasks={mesh_tasks} remesh_tasks={remesh_tasks} remesh_geometry={remesh_geometry} remesh_lighting={remesh_lighting} remesh_fluid={remesh_fluid} mesh_assets={mesh_assets} images={image_assets} file_images={file_images} runtime_images={runtime_images} non_font_runtime_images={non_font_runtime_images} runtime_top_shapes={runtime_top_shapes:?} font_atlas_keys={font_atlas_keys} font_atlases={font_atlas_count} font_atlas_bytes={font_atlas_bytes} deltas={deltas:?} standard_materials={} terrain_materials={}",
         assets.state.get(),
+        async_timings.generation,
+        async_timings.initial_mesh,
+        async_timings.remesh,
         assets.standard_materials.len(),
         assets.terrain_materials.len(),
     );
