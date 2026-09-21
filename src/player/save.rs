36384888ce2a2f4688437426711a55eb6e43bdee
Sara Borges <sarakborges@outlook.com>
2026-09-21 11:39:47 -0300

    feat: carry flight state in in-memory player save

--- src/player/save.rs
+++ src/player/save.rs
@@ -8,6 +8,7 @@ pub(crate) struct PlayerSaveData {
     game_mode: GameMode,
     health: Option<f32>,
     look: Option<(f32, f32)>,
+    flying: bool,
 }
 
 impl PlayerSaveData {
@@ -27,16 +28,22 @@ impl PlayerSaveData {
         self.look
     }
 
+    pub(crate) fn flying(&self) -> bool {
+        self.flying
+    }
+
     pub(crate) fn save_with_health(
         &mut self,
         position: Vec3,
         game_mode: GameMode,
         health: Option<f32>,
         look: Option<(f32, f32)>,
+        flying: bool,
     ) {
         self.position = Some(position);
         self.game_mode = game_mode;
         self.health = health;
         self.look = look;
+        self.flying = flying;
     }
 }
