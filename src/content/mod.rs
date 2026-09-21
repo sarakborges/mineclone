d6bfa92235991fd346d0868a0aeb48f8ed40fbae
Sara Borges <sarakborges@outlook.com>
2026-09-21 15:20:42 -0300

    Register structure set content module

--- src/content/mod.rs
+++ src/content/mod.rs
@@ -34,6 +34,7 @@ pub(crate) mod secondary_property;
 pub(crate) mod sky;
 pub(crate) mod structure;
 pub(crate) mod structure_rules;
+pub(crate) mod structure_set;
 pub(crate) mod tool;
 pub(crate) mod tool_id;
 mod validation;
