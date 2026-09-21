af60dc9a12c9f737a5a3ea2b52830a0a01dfad5b
Sara Borges <sarakborges@outlook.com>
2026-09-21 13:16:14 -0300

    cleanup: import persistent chunk coordinate type

--- src/voxel/world/persistence.rs
+++ src/voxel/world/persistence.rs
@@ -1,5 +1,7 @@
 use std::{io, sync::Arc};
 
+use bevy::prelude::IVec3;
+
 use crate::content::{block::BlockRegistry, fluid::FluidRegistry, layer::LayerRegistry};
 use crate::voxel::chunk_disk::DiskChunk;
 
