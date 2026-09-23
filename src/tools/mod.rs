f64e100501727e573ea3f53907787f553102269e
Sara Borges <sarakborges@outlook.com>
2026-09-23 15:32:48 -0300

    Format tool plugin registration

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,13 +1,13 @@
+mod artisans_kit;
 mod brush;
 mod carpenters_axe;
-mod artisans_kit;
 mod shears;
 mod structure_tool;
 
+use artisans_kit::ArtisansKitPlugin;
 use bevy::prelude::*;
 use brush::BrushPlugin;
 use carpenters_axe::CarpentersAxePlugin;
-use artisans_kit::ArtisansKitPlugin;
 use shears::ShearsPlugin;
 use structure_tool::StructureToolPlugin;
 
@@ -17,6 +17,12 @@ pub(crate) struct ToolsPlugin;
 
 impl Plugin for ToolsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((BrushPlugin, CarpentersAxePlugin, ArtisansKitPlugin, ShearsPlugin, StructureToolPlugin));
+        app.add_plugins((
+            ArtisansKitPlugin,
+            BrushPlugin,
+            CarpentersAxePlugin,
+            ShearsPlugin,
+            StructureToolPlugin,
+        ));
     }
 }
