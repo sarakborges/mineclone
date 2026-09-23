ddac3a626f724e4485836e0f280ade3ac8d77bb4
Sara Borges <sarakborges@outlook.com>
2026-09-23 14:58:20 -0300

    Expose generic item ID module

--- src/content/mod.rs
+++ src/content/mod.rs
@@ -25,6 +25,7 @@ pub(crate) mod dimension_hydrology;
 pub(crate) mod fluid;
 pub(crate) mod inventory_category;
 pub(crate) mod item;
+pub(crate) mod item_id;
 pub(crate) mod layer;
 pub(crate) mod layer_id;
 pub(crate) mod player;
