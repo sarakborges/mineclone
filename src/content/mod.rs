fa4e776145f6bd09129369bf5d12344e07820ae9
Sara Borges <sarakborges@outlook.com>
2026-09-23 11:39:15 -0300

    Register shared item content helpers

--- src/content/mod.rs
+++ src/content/mod.rs
@@ -24,6 +24,7 @@ pub(crate) mod dimension;
 pub(crate) mod dimension_hydrology;
 pub(crate) mod fluid;
 pub(crate) mod inventory_category;
+pub(crate) mod item;
 pub(crate) mod layer;
 pub(crate) mod layer_id;
 pub(crate) mod player;
