968132777eb067b81748bfe34efaea6f582a9a93
Sara Borges <sarakborges@outlook.com>
2026-09-23 14:04:54 -0300

    Remove unused attack registry iterator

--- src/content/attack.rs
+++ src/content/attack.rs
@@ -71,8 +71,4 @@ impl AttackRegistry {
     pub fn get(&self, id: &str) -> Option<&AttackDefinition> {
         self.definitions.get(id)
     }
-
-    pub fn iter(&self) -> impl Iterator<Item = &AttackDefinition> {
-        self.definitions.values()
-    }
 }
